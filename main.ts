1ca41bfc4a5b8f11d787084bda5a5796e48a9555
Saba Samad <164026551+Sabakahn@users.noreply.github.com>
2024-06-05 02:52:15 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+console.log("Wellcome to CLI-Saba-Guessing-Game");
+
+const randomNumber = Math. floor(Math.random()*100+1);
+const answer=await inquirer.prompt([
+    {
+        name:"userGuessedNumber",
+        type: "number",
+        message:"Pleaze Guess a number",
+    }
+]);
+if (answer.userGuessedNumber === randomNumber){
+    console.log("Congretulations! you guessed a right number.")
+}else{
+    console.log("Sorry! you guessed wrong number.")
+};
